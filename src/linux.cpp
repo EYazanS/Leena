866c703782135d6d5167f948e9331e7bb1894dea
Yazan Salameh <yazan@serenbit.ps>
2022-03-17 14:02:49 +0200

    Added sdl and launch task for linux

--- src/linux.cpp
+++ src/linux.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
-
-using namespace std;
+#include <SDL2/SDL.h>
 
 int main(int args, char *argv[])
 {
-    cout << "Hello linux\n";
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        std::cout << "Failed to initialize the SDL2 library\n";
+        std::cout << "Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Leena game engine",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        680, 480,
+        0);
+
+    if (!window)
+    {
+        std::cout << "Failed to create window\n";
+        return -1;
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    if (!window_surface)
+    {
+        std::cout << "Failed to get the surface from the window\n";
+        return -1;
+    }
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
 
     return 0;
 }
